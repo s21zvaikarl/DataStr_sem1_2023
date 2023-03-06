8d8fdfd2caa2b45cbfe2331118f57ddebf79761b
Kārlis Zvaigzne <s21zvaikarl@vea.lv>
2023-03-06 11:49:06 +0200

    add: a lot

--- src/datastr/MyArrayList.java
+++ src/datastr/MyArrayList.java
@@ -1,197 +1,253 @@
 package datastr;
 
-public class MyArrayList {
-	private char[] elements;
+public class MyArrayList<T> {
+	private T[] elements;
 	private final int DEFAULT_ARRAY_SIZE = 6;
 	private int arraySize = DEFAULT_ARRAY_SIZE;
 	private int elementCounter = 0;
-	
-	//no arg constructor
+
+	// no-args kontruktors
 	public MyArrayList() {
-		elements = new char[arraySize]; //masivs ar 6 sunam
+		elements = (T[]) new Object[arraySize]; // will be with size 6 (DEFAULT_ARRAY_SIZE)
 	}
-	//arg constructor
+
+	// args konstruktors
 	public MyArrayList(int inputArraySize) {
-		if(inputArraySize > 0) {
+		if (inputArraySize > 0) {
 			arraySize = inputArraySize;
 		}
-		elements = new char[arraySize];
+
+		elements = (T[]) new Object[arraySize];
 	}
-	
+
+	// TODO create isFull and isEmpty functions
 	public boolean isEmpty() {
+		// long if-else
 		/*
-		if(elementCounter == 0) {
-			return true;
-		}
-		return false;
-		*/
-		//return (elementCounter == 0)? true : false;
-		
+		 * if(elementCounter == 0) { return true; } else { return false; }
+		 */
+		// for example
+		/*
+		 * return (arraySize< elementCounter) ? "var ievietot" : "nevar ievietot"; if
+		 * (arraySize< elementCounter) { return "var ievietot" ; } else { return
+		 * "nevar ievietot"; }
+		 */
+
+		// short if-else
+		// kopējais (if izteiksme) ? ko darīt true : ko darīt false gadījumā;
+		// return (elementCounter == 0)? true : false;
+
 		return (elementCounter == 0);
+
 	}
-	
+
 	public boolean isFull() {
 		return (elementCounter == arraySize);
 	}
-	
+
 	public int howManyElements() {
 		return elementCounter;
 	}
-	
+
+	// 1. create a definition of function increaseArray
 	private void increaseArray() {
-		int newArraySize = (arraySize>100)? (int) (arraySize*1.5) : arraySize*2;
-		char[] newElements = new char[newArraySize];
-		for(int i = 0; i<arraySize; i++) {
+		// 2. calculate the new size of the new array
+		int newArraySize = (arraySize > 100) ? (int) (arraySize * 1.5) : arraySize * 2;
+
+		/*
+		 * int newArraySize = arraySize *2; if(arraySize>100) { newArraySize = (int)
+		 * (arraySize*1.5); }
+		 */
+		/*
+		 * int newArraySize; if(arraySize > 100) { newArraySize = (int)(arraySize *1.5);
+		 * } else { newArraySize =arraySize * 2; }
+		 */
+		// 3. create the new array
+		T[] newElements = (T[]) new Object[newArraySize];
+		// 4. copy all elements from the old array to the new array
+		for (int i = 0; i < elementCounter; i++) {
 			newElements[i] = elements[i];
 		}
+		// 5. change reference to the new array and it's size
 		elements = newElements;
 		arraySize = newArraySize;
 	}
-	
-	public void add(char newElement) {
+
+	// 1. create a definition of function add
+	public void add(T newElement) {
+		// 2. verify if the array is full
 		if (isFull()) {
+			// 2.1. call an increaseArray() func.
 			increaseArray();
 		}
-		elements[elementCounter++] = newElement;
-		//elementCounter++;
+
+		// 3. add the new element in the array
+		// the non-optimize option
+		elements[elementCounter] = newElement;
+		elementCounter++;
+		// the optimize option
+		// elements[elementCounter++] = newElement;
+
+		// 4. increase elementCounter
 	}
-	
-	public void add(char newElement, int index) throws Exception {
+
+	// TODO
+	// 1. create a definition of function add
+	public void add(T newElement, int index) throws Exception {
+		// 2. verify the index - is it appropriate
 		if (index >= 0 && index <= elementCounter) {
-			if (isFull()) {
-				increaseArray();
-			}
-			for(int i = elementCounter; i>index; i--) {
-				elements[i] = elements[i-1];
-			}
-			elements[index] = newElement;
-			elementCounter++;
-		}
-		else {
-			throw (new Exception("Wrong index"));
-		}
-	}
-	public void remove(int index) throws Exception {
-		if (isEmpty()) {
-			throw (new Exception("Nothing to delete"));
-		}
-		else {
-			if (index >= 0 && index <= elementCounter) {
-				for(int i = index; i<elementCounter; i++) {
-					elements[i] = elements[i+1];
+			// 3.5 verify if the index is equal with elementCounter
+			if (index == elementCounter) {
+				add(newElement);
+			} else {
+				// 3. verify isFull
+				if (isFull()) {
+					increaseArray();
 				}
-				elements[elementCounter-1] = 0;
-				elementCounter--;
+				// 4. copy from the end to the right side (using the index)
+				for (int i = elementCounter; i > index; i--) {
+					elements[i] = elements[i - 1];
+				}
+				// 5. add the new element in the specified index
+				elements[index] = newElement;
+				// 6. increase elementCounter
+				elementCounter++;
 			}
-			else {
-				throw (new Exception("Wrong index"));
-			}
-		}
-	}
-	public int get(int index) throws Exception {
-		if (isEmpty()) {
-			throw (new Exception("Nothing to return"));
-		}
-		if (index >= 0 && index <= elementCounter) {
-			return elements[index];
-		}
-		else {
+
+		} else {
 			throw (new Exception("Wrong index"));
 		}
 	}
-	public boolean find(char newElement) throws Exception {
+
+	// 1. create a definition of function add
+	public void remove(int index) throws Exception {
+		// 2. verify isEmpty
 		if (isEmpty()) {
-			throw (new Exception("Nothing to find"));
+			throw (new Exception("Array is empty and it is not possible to remove elements"));
+		} else {
+			// 3. verify the index - is it not appropriate
+			if (index < 0 || index >= elementCounter) {
+				throw (new Exception("Wrong index"));
+			} else {
+				// 4. copy elements from index to end
+				for (int i = index; i < elementCounter - 1; i++) {
+					elements[i] = elements[i + 1];
+				}
+				// 5. initialize the last element to null reference
+				elements[elementCounter - 1] = null;
+
+				// 6. decrease elementCounter
+				elementCounter--;
+
+				// for optimization
+				// elements[--elementCounter] = 0;
+			}
 		}
-		for (int i=0; i < elementCounter; i++) {
-			if(newElement == elements[i]) {
+	}
+
+	public T retrieve(int index) throws Exception {
+		if (isEmpty()) {
+			throw (new Exception("Array is empty and it is not possible to retrieve elements"));
+		} else {
+			if (index < 0 || index >= elementCounter) {
+				throw (new Exception("Wrong index"));
+			} else {
+				return elements[index];
+			}
+		}
+	}
+
+	public boolean search(T inputElement) {
+
+		for (int i = 0; i < elementCounter; i++) {
+			if (elements[i].equals(inputElement)) {
 				return true;
 			}
 		}
+
 		return false;
+
 	}
-	
-	public char[] retrieveNextNeighbours(char inputElement) throws Exception {
-		if(find(inputElement)) {
+
+	// TODO retrieveNextNeigbour
+	public T[] retrieveNextNeigbours(T inputElement) throws Exception {
+		// true case
+		if (search(inputElement)) {
 			int howManySearchedElements = 0;
-			for(int i = 0; i < elementCounter; i++) {
-				if(elements[i] == inputElement) {
+			for (int i = 0; i < elementCounter; i++) {
+				if (elements[i].equals(inputElement)) {
 					howManySearchedElements++;
 				}
 			}
-				if(elements[elementCounter-1]==inputElement) {
-					howManySearchedElements--;
+			if (elements[elementCounter - 1].equals(inputElement)) {
+				howManySearchedElements--;
+			}
+
+			T[] nextNeigbours = (T[]) new Object[howManySearchedElements];
+			int indexForNeigbors = 0;
+			for (int i = 0; i < elementCounter - 1; i++) {
+				if (elements[i].equals(inputElement)) {
+					nextNeigbours[indexForNeigbors] = elements[i + 1];
+					indexForNeigbors++;
 				}
-				char[] nextNeighbours = new char[howManySearchedElements];
-				int indexForNeighbours = 0;
-				for(int j = 0; j < elementCounter-1; j++) {
-					if(elements[j] == inputElement) {
-						nextNeighbours[indexForNeighbours] = elements[j+1];
-						indexForNeighbours++;
-					}
-				}
-			
-		return nextNeighbours;	
+			}
+			return nextNeigbours;
+
 		}
+		// false case
 		else {
-			throw (new Exception("Input element is not found"));
+			throw (new Exception("Input element is not found in the array"));
 		}
-		
-		
 	}
 
-	public char[] sort(SortingType type) throws Exception {
-		if(isEmpty()) {
-			throw (new Exception("Nothing to sort"));
-		}
-		else {
-			char[] sortArray = new char[elementCounter];
-			for(int i = 0; i < elementCounter; i++) {
+	public T[] sort(SortingType type) throws Exception {
+
+		if (isEmpty()) {
+			throw (new Exception("Array is empty and it is not possible to sort"));
+		} else {
+			T[] sortArray = (T[]) new Object[elementCounter];
+
+			for (int i = 0; i < elementCounter; i++) {
 				sortArray[i] = elements[i];
 			}
-			if(type == SortingType.ASC) {
-				for(int i = 0; i < elementCounter; i++) {
-					for(int j = 0; j < elementCounter; j++) {
-						if(sortArray[i] > sortArray[j]) {
-							char temp = sortArray[i];
-							sortArray[i] = sortArray[j];
-							sortArray[j] = temp;
-						}
-						
+
+			int sortVariable = 1; // DESC
+			if (type == SortingType.ASC) {
+				sortVariable = -1;
+			}
+
+			for (int i = 0; i < elementCounter; i++) {
+				for (int j = 0; j < elementCounter; j++) {
+					// if(sortArray[i] < sortArray[j] )
+					if (((Comparable) (sortArray[i])).compareTo(sortArray[j]) == sortVariable) { // 0 6
+						T temp = sortArray[i];
+						sortArray[i] = sortArray[j];
+						sortArray[j] = temp;
 					}
 				}
 			}
-			else if(type == SortingType.DESC) {
-				for(int i = 0; i < elementCounter; i++) {
-					for(int j = 0; j < elementCounter; j++) {
-						if(sortArray[i] < sortArray[j]) {
-							char temp = sortArray[i];
-							sortArray[i] = sortArray[j];
-							sortArray[j] = temp;
-						}
-						
-					}
-				}
-			}
-			else {
-				throw (new Exception("Wrong sorting type"));
-			}
+
 			return sortArray;
 		}
+
 	}
-	
+
 	public void print() throws Exception {
 		if (isEmpty()) {
-			throw (new Exception("Nothing to print"));
+			throw (new Exception("Array is empty and it is not possible to print elements"));
+		} else {
+			for (int i = 0; i < elementCounter; i++) {
+				System.out.print(elements[i] + " ");
+			}
+			System.out.println();
 		}
-		for (int i=0; i < elementCounter; i++) {
-			System.out.print(elements[i] + " ");
-		}
-		System.out.println();
 	}
-	public void clear() {
+
+	public void makeEmpty() {
 		arraySize = DEFAULT_ARRAY_SIZE;
 		elementCounter = 0;
-		elements = new char[arraySize];
+		elements = (T[]) new Object[arraySize];
+		System.gc();
 	}
+
 }
--- src/service/MainService.java
+++ src/service/MainService.java
@@ -3,39 +3,44 @@ package service;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.util.Arrays;
 import java.util.Scanner;
 
 import datastr.MyArrayList;
 import datastr.SortingType;
+import model.Student;
+import model.Faculty;
 
 public class MainService {
-	public static void main(String[] args) {
+
+	public static void main(String[] args){
 		MyArrayList charList = new MyArrayList();
-		try {
-			charList.add('a');
-			charList.add('c');
-			charList.add('n');
-			charList.add('z', 0);
-			charList.print();
-			System.out.println(charList.howManyElements());
-			charList.remove(0);
-			charList.print();
-			System.out.println(charList.get(1));
-			System.out.println("Search: " + charList.find('n'));
-			charList.add('d');
-			charList.add('z');
-			charList.add('n');
-			charList.add('z');
-			System.out.println(Arrays.toString(charList.retrieveNextNeighbours('n')));
-			System.out.println(charList.sort(SortingType.ASC));
-			charList.print();
-			charList.clear();
-			//charList.print();
-			charList.add('d');
-			charList.print();
+		try
+		{
+			charList.add('a'); //a
+			charList.add('f'); //a f
+			charList.add('b'); //a f b
+			charList.add('z', 0); //z a f b
+			charList.print(); // z a f b
+			System.out.println(charList.howManyElements());//4
+			charList.remove(1);//z f b
+			charList.print();//z f b
+			System.out.println(charList.retrieve(1)); //f
+			System.out.println("Search: " + charList.search('š'));//true
+			charList.add('d');//z f b d
+			charList.add('z');//z f b d z
+			charList.add('a');//z f b d z a
+			charList.add('z');//z f b d z a z  increase() will be called
+			System.out.println(Arrays.toString(charList.retrieveNextNeigbours('z')));//f a
+			System.out.println(Arrays.toString(charList.sort(SortingType.ASC))); //a b d f z z z 
+			charList.print();//z f b d z a z
+			charList.makeEmpty();//empty array
+			//charList.print();//empty array
+			charList.add('d');//d
+			charList.print();//d
 			
-			System.out.println("~~~~~~~~~~~~~~~~~~~");
+			System.out.println("---------------------------------");
 			MyArrayList fileList = getArrayElementsFromFile("resources/numbers.txt");
 			
 			fileList.print();
@@ -44,19 +49,33 @@ public class MainService {
 			fileList.print();
 			System.out.println(Arrays.toString(fileList.sort(SortingType.ASC)));
 			
+			MyArrayList<String> stringList = new MyArrayList<>();
+			stringList.add("Karina");
+			stringList.add("Janis");
+			stringList.add("Zanis", 0);
+			stringList.print();//Žanis, Karina, Jānis
+			System.out.println(Arrays.toString(stringList.sort(SortingType.DESC)));
+			stringList.remove(1);
+			stringList.print();//Žanis, Jānis
+			
+			MyArrayList<Student> studentList = new MyArrayList<>();
+			
+			
+			
+		}
+		catch (Exception e) {
+			System.out.println(e);
+		}
 
-		}
-		catch(Exception e) {
-			e.printStackTrace();
-		}
-		
 	}
 	
 	public static MyArrayList getArrayElementsFromFile(String path) throws FileNotFoundException {
+		
 		File myFile = new File(path);
 		FileInputStream myInputStream = new FileInputStream(myFile);
 		Scanner myScanner = new Scanner(myInputStream);
 		MyArrayList listFromFile = new MyArrayList();
+		
 		while(myScanner.hasNextLine()) {
 			String line = myScanner.nextLine();
 			char element = line.charAt(0);
@@ -64,5 +83,7 @@ public class MainService {
 		}
 		myScanner.close();
 		return listFromFile;
+		
 	}
+
 }
